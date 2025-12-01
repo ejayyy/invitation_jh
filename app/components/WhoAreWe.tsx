731a3e1a307d625c9ce15f90033c99fd068c1e79
ejayyy <ejay0811@gmail.com>
2025-12-01 18:03:01 +0900

    Add: who we are

---
+++ app/components/WhoAreWe.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export default function WhoAreWe() {
+  return (
+    <section>
+      <div className="relative aspect-[5/4] w-full">
+        <Image
+          src="/whoweare/main.jpg"
+          alt="Who are we"
+          fill
+          className="object-cover rounded-lg"
+        />
+      </div>
+      <div className="flex flex-col gap-1 w-fit mx-auto mt-4">
+        <div className="grid grid-cols-[repeat(6,auto)] gap-2">
+          <span>하후동</span><span>·</span><span>박옥분</span><span>의</span><span>아들</span><span>하정수</span>
+        </div>
+        <div className="grid grid-cols-[repeat(6,auto)] gap-2">
+          <span>정동수</span><span>·</span><span>박미경</span><span>의</span><span>딸</span><span>정보민</span>
+        </div>
+      </div>
+    </section>
+  );
+}
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Greeting from "./components/Greeting";
+import WhoAreWe from "./components/WhoAreWe";
 
 export default function Home() {
   return (
     <div className="w-full max-w-3xl mx-auto px-7">
       <Greeting />
+      <WhoAreWe />
     </div>
   );
 }
