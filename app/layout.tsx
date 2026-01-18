896cf4e3c75eb48740fc838412dfdc71f2c2015e
johnny.han <johnny.han@kakaocorp.com>
2026-01-18 15:08:41 +0900

    Add: openGraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,32 @@ const gowunBatang = Gowun_Batang({
 export const metadata: Metadata = {
   title: "정수 보민 청첩장",
   description: "How long time no see us!",
+  metadataBase: new URL("https://jeongsubomin.life"),
+  openGraph: {
+    title: "정수 보민 청첩장",
+    description: "How long time no see us!",
+    url: "https://jeongsubomin.life",
+    siteName: "정수 보민 청첩장",
+    images: [
+      {
+        url: "/greeting/main.jpg",
+        width: 1200,
+        height: 630,
+        alt: "정수 보민 결혼식 초대장",
+      },
+    ],
+    locale: "ko_KR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "정수 보민 청첩장",
+    description: "How long time no see us!",
+    images: ["/greeting/main.jpg"],
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
